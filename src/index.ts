a3aca889f5b48d7f8ab1b1ad4bd8679cbd5a2068
minung.han <hmu332233@gmail.com>
2021-11-27 20:44:46 +0900

    Add peer cursor feature example

--- src/index.ts
+++ src/index.ts
@@ -4,11 +4,38 @@ const PEER_PREFIX = '@peer-whiteboard';
 
 const myPeerId = 'minung';
 
+const app = document.getElementById('app');
+const myCursor = document.getElementById('my-cursor');
+const peerCursor = document.getElementById('peer-cursor');
 
 const myPeer = new Peer();
+let peerConnection;
+
+app.addEventListener("mousemove", updateDisplay, false);
+app.addEventListener("mouseenter", updateDisplay, false);
+app.addEventListener("mouseleave", updateDisplay, false);
+
+function updateDisplay(event) {
+  // console.log(event.pageX, event.pageY);
+  myCursor.style.left = event.pageX;
+  myCursor.style.top = event.pageY;
+
+  peerConnection && peerConnection.send({ event: 'mousemove', payload: { pageX: event.pageX, pageY: event.pageY }});
+}
+
+
 
 // TODO: socket을 별도로 이용해서 모든 peer끼리 데이터를 공유할 수 있도록 하기
-const peers = {};
+const peers = {
+  ids: [],
+  entities: {},
+};
+
+function addPeer(data) {
+  const { peer } = data;
+  peers.ids.push(peer);
+  peers.entities[peer] = data;
+}
 
 myPeer.on('open', (id) => {
   console.log('open', id);
@@ -17,19 +44,31 @@ myPeer.on('open', (id) => {
 myPeer.on('connection', function(conn) {
   console.log(conn, 'connected!');
   conn.on('data', function(data){
-    console.log(data);
+    // console.log(data);
+    const { event, payload } = data;
+    const { pageX, pageY } = payload;
+
+    peerCursor.style.left = pageX;
+    peerCursor.style.top = pageY;
   });
+  peerConnection = conn;
 });
 
 
-let conn;
-
-window.test = (id) => {
-  // 타인과 연결
-  conn = myPeer.connect(id);
-  // on open will be launch when you successfully connect to PeerServer
+window.connect = (id) => {
+  const conn = myPeer.connect(id);
   conn.on('open', function(){
-    // here you have conn.id
-    conn.send('hi!');
+  });
+  peerConnection = conn;
+  // console.log(conn)
+
+  conn.on('data', function(data){
+    // console.log(data);
+    const { event, payload } = data;
+    const { pageX, pageY } = payload;
+
+    peerCursor.style.left = pageX;
+    peerCursor.style.top = pageY;
   });
 }
+
